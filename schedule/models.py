6bced7ee86d3cd14af92bcddf7d1930f6fd55fea
Roman Sokolov <gnonasis@gmail.com>
2024-12-03 10:51:25 +0200

    add models Tag & Task

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Tags"
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name="tasks")
+
+    class Meta:
+        verbose_name_plural = "Tasks"
+        ordering = ["-deadline"]
